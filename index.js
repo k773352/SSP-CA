cfb747a8fc2845fe6e6aa701f5686ae260c5bea2
Ruby <k773352@gmail.com>
2022-07-29 19:09:53 +0000

    add some code

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ router.get('/get/html', function(req, res) {
 
        let doc = xmlParse(xml),
            stylesheet = xmlParse(xsl);
-           
+
+       let result = xsltProcess(doc, stylesheet);
+
+       res.end(result.toString());   
+        
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
